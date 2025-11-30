78b6ef8b77e983009e07e73ce2af4acb63ed8eab
Zahra Rassaf <98451722+Zahrarasaf@users.noreply.github.com>
2025-11-30 15:35:10 +0330

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,3 @@
-# Remove the problematic import line that says:
-# from .config import config
-
-# Keep only these imports:
 from .spe9_data_parser import SPE9DataParser
 from .feature_engineer import FeatureEngineer
 from .ensemble_model import DeepEnsembleModel
