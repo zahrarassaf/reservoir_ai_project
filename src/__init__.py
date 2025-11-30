71bb44c1ba5abf66cabf2ff932606aeb6aabcac0
Zahra Rassaf <98451722+Zahrarasaf@users.noreply.github.com>
2025-11-30 15:24:24 +0330

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,21 +1,12 @@
-"""
-RESERVOIR AI PACKAGE
-"""
-from .config import config
-from .data_loader import ReservoirDataLoader
-from .feature_engineer import ReservoirFeatureEngineer
-from .ensemble_model import AdvancedReservoirModel
-from .evaluator import ModelEvaluator
-from .utils import save_predictions, setup_directories, get_project_info
+# Remove the problematic import, keep it simple
+from .spe9_data_parser import SPE9DataParser
+from .feature_engineer import FeatureEngineer
+from .ensemble_model import DeepEnsembleModel
+from .ensemble_trainer import EnsembleTrainer
 
-__version__ = "1.0.0"
 __all__ = [
-    'config',
-    'ReservoirDataLoader',
-    'ReservoirFeatureEngineer', 
-    'AdvancedReservoirModel',
-    'ModelEvaluator',
-    'save_predictions',
-    'setup_directories',
-    'get_project_info'
+    'SPE9DataParser',
+    'FeatureEngineer', 
+    'DeepEnsembleModel',
+    'EnsembleTrainer'
 ]
