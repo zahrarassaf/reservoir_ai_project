a5a1222f745648418eb927f30a405a44b7465ef4
Zahra Rassaf <98451722+Zahrarasaf@users.noreply.github.com>
2025-12-02 18:46:10 +0330

    Update __init__.py

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,14 +1,9 @@
-"""
-Analysis tools for SPE9 simulation results
-"""
 
-__version__ = "1.0.0"
-__author__ = "Zahra Rasaf"
+"""
+Analysis modules for reservoir simulation.
+"""
 
 from .performance_calculator import PerformanceCalculator
 from .plot_generator import PlotGenerator
 
-__all__ = [
-    "PerformanceCalculator",
-    "PlotGenerator"
-]
+__all__ = ['PerformanceCalculator', 'PlotGenerator']
