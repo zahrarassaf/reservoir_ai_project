b6e9b673f72fc628f84bf242350ab7f51701d556
Zahra Rassaf <98451722+Zahrarasaf@users.noreply.github.com>
2025-12-02 18:45:51 +0330

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,18 +1,10 @@
-"""
-SPE9 Reservoir Simulation Package
-Professional implementation of SPE9 benchmark
-"""
 
-__version__ = "1.0.0"
-__author__ = "Zahra Rassaf"
-__email__ = "zahrarasaf@yahoo.com"
+"""
+Source modules for reservoir simulation.
+"""
 
 from .simulation_runner import SimulationRunner
 from .results_processor import ResultsProcessor
 from .data_validator import DataValidator
 
-__all__ = [
-    "SimulationRunner",
-    "ResultsProcessor", 
-    "DataValidator"
-]
+__all__ = ['SimulationRunner', 'ResultsProcessor', 'DataValidator']
