75c3c21ee127c82e8639b522af43b48f05c4b470
Zahra Rassaf <98451722+Zahrarasaf@users.noreply.github.com>
2025-12-04 01:07:13 +0330

    Delete src directory

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Source modules for reservoir simulation.
-"""
-
-from .simulation_runner import SimulationRunner
-from .results_processor import ResultsProcessor
-from .data_validator import DataValidator
-
-__all__ = ['SimulationRunner', 'ResultsProcessor', 'DataValidator']
