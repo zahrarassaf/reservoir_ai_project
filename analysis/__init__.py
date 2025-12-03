be38c3118476c66bffb14de461c091a489e82fdf
Zahra Rassaf <98451722+Zahrarasaf@users.noreply.github.com>
2025-12-04 01:21:13 +0330

    Delete analysis directory

--- analysis/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Analysis modules for reservoir simulation.
-"""
-
-from .performance_calculator import PerformanceCalculator
-from .plot_generator import PlotGenerator
-
-__all__ = ['PerformanceCalculator', 'PlotGenerator']
