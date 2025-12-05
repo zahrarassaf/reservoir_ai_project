b6679535b29d121a2c6f514126c1b30049efc684
Zahra Rassaf <98451722+Zahrarasaf@users.noreply.github.com>
2025-12-06 00:20:43 +0330

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,21 +1,4 @@
-"""
-Reservoir Simulation PhD Package
-"""
+from .data_loader import ReservoirData
+from .simulator import ReservoirSimulator, SimulationParameters
 
-__version__ = "1.0.0"
-__author__ = "Reservoir Engineering Team"
-
-from .data_loader import GoogleDriveLoader, ReservoirData
-from .simulator import ReservoirSimulator
-from .analyzer import ReservoirAnalyzer
-from .visualizer import ReservoirVisualizer
-from .economics import EconomicAnalyzer
-
-__all__ = [
-    "GoogleDriveLoader",
-    "ReservoirData",
-    "ReservoirSimulator",
-    "ReservoirAnalyzer",
-    "ReservoirVisualizer",
-    "EconomicAnalyzer",
-]
+__all__ = ['ReservoirData', 'ReservoirSimulator', 'SimulationParameters']
