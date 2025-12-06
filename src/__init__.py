c054bcfde87d82345d40d35ed39d54547e36c3f4
Zahra Rassaf <98451722+Zahrarasaf@users.noreply.github.com>
2025-12-06 18:02:41 +0330

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,9 @@
-
 from .data_loader import DataLoader
 from .economics import ReservoirSimulator, SimulationParameters
+
+try:
+    from .visualizer import Visualizer
+except ImportError:
+    pass
+
 __all__ = ['DataLoader', 'ReservoirSimulator', 'SimulationParameters']
