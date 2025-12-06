5d38a035403cf631eb51376c0ba364094b6f49d5
Zahra Rassaf <98451722+Zahrarasaf@users.noreply.github.com>
2025-12-06 19:04:21 +0330

    Delete src/__init__.py

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-from .data_loader import DataLoader
-from .economics import ReservoirSimulator, SimulationParameters
-
-try:
-    from .visualizer import Visualizer
-except ImportError:
-    pass
-
-__all__ = ['DataLoader', 'ReservoirSimulator', 'SimulationParameters']
